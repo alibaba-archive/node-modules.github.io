bd423875402da013bd07c5e64e4cba2c926041d6
fengmk2 <fengmk2@gmail.com>
2014-04-04 12:29:59 +0800

    first commit

---
+++ test/book.test.js
@@ -0,0 +1,22 @@
+/**!
+ * book - test/book.test.js
+ *
+ * Copyright(c) 2014 fengmk2 and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var book = require('../');
+
+describe('book.test.js', function () {
+
+});
